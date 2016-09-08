1a18068cb539c174c631414a2c4671711dc8db1b
mrlyceist <dorogov.alexey@gmail.com>
2016-09-09 02:11:24 +0500

    transfer commit

---
+++ DeployToFtp/FtpClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployToFtp
+{
+    class FtpClient
+    {
+        private FtpWebRequest _ftpRequest;
+        private FtpWebResponse _ftpResponce;
+
+        public string Host { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool UseSsl { get; set; }
+
+        public FileStruct[] ListDirectory(string directory)
+        {
+            if (directory == null || directory == string.Empty)
+                directory = "/";
+
+            _ftpRequest = (FtpWebRequest)WebRequest.Create($"ftp://{Host}{directory}");
+            _ftpRequest.Credentials = new NetworkCredential(UserName, Password);
+            _ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            _ftpRequest.EnableSsl = UseSsl;
+
+            _ftpResponce = (FtpWebResponse)_ftpRequest.GetResponse();
+
+            string content = string.Empty;
+
+            StreamReader reader = new StreamReader(_ftpResponce.GetResponseStream(), Encoding.ASCII);
+            content = reader.ReadToEnd();
+            reader.Close();
+            _ftpResponce.Close();
+
+            DirectoryListParser parser = new DirectoryListParser(content);
+            return parser.FullListing;
+        }
+
+    }
+
+    struct FileStruct
+    {
+        public string Flags;
+        public string Owner;
+        public bool IsDirectory;
+        public string CreateTime;
+        public string Name;
+    }
+}
--- DeployToFtp/Program.cs
+++ DeployToFtp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,34 @@ namespace DeployToFtp
 {
     class Program
     {
+        private static string _userName = "bit";
+        private static string _password = "18071978";
+        //private static string _host = "ftp://ursa.ooobit.com";///software";
+        private static string _host = "ftp://ursa.ooobit.com/software";
+
         static void Main(string[] args)
         {
+            var ftpRequest = (FtpWebRequest)WebRequest.Create($"{_host}/wat");
+            ftpRequest.Credentials = new NetworkCredential(_userName, _password);
+            //ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            ftpRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+            ftpRequest.EnableSsl = false;
+
+            var ftpResponce = (FtpWebResponse)ftpRequest.GetResponse();
+            ftpResponce.Close();
+            string cont = string.Empty;
+
+            ftpRequest = (FtpWebRequest)WebRequest.Create(_host);
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            ftpResponce = (FtpWebResponse)ftpRequest.GetResponse();
+            StreamReader reader = new StreamReader(ftpResponce.GetResponseStream(), Encoding.ASCII);
+            cont = reader.ReadToEnd();
+            reader.Close();
+            ftpResponce.Close();
+
+            Console.WriteLine(cont);
+            Console.ReadLine();
         }
     }
 }
